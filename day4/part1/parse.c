cfa3f314e0710ddaff45e4f0d58134fae7d8c4e0
Glenn Winnink <gwinnink@student.codam.nl>
2022-12-06 11:43:29 +0100

    completed day 4 part 1

---
+++ day4/part1/parse.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gwinnink <gwinnink@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/06 11:20:06 by gwinnink          #+#    #+#             */
+/*   Updated: 2022/12/06 11:31:34 by gwinnink         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	check_overlap(char *str)
+{
+	char	**range = ft_split(str, ',');
+	char	**e1 = ft_split(range[0], '-');
+	char	**e2 = ft_split(range[1], '-');
+	int		ret = 0;
+
+	if (ft_atoi(e1[0]) <= ft_atoi(e2[0]) && ft_atoi(e1[1]) >= ft_atoi(e2[1]))
+		ret++;
+	else if (ft_atoi(e2[0]) <= ft_atoi(e1[0]) && ft_atoi(e2[1]) >= ft_atoi(e1[1]))
+		ret++;
+	ft_free_all(e1);
+	ft_free_all(e2);
+	ft_free_all(range);
+	return (ret);
+}
