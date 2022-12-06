bf55cd7c543fc2e4dbf0eb90fc5afc98a5588ecc
Glenn Winnink <gwinnink@student.codam.nl>
2022-12-06 15:48:09 +0100

    completed day 6 part 2

---
+++ day6/part2/main.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gwinnink <gwinnink@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/01 11:15:20 by gwinnink          #+#    #+#             */
+/*   Updated: 2022/12/06 15:16:23 by gwinnink         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "libft.h"
+
+
+int	check_marker(char *str)
+{
+	int	i = 0, j;
+
+	while (str[i] && i < 14)
+	{
+		j = i + 1;
+		while (str[j] && j < 14)
+		{
+			if (str[i] == str[j])
+			{
+				return (0);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	main(void)
+{
+	int			fd;
+	int			i = 0;
+	char		*line;
+	int			result = 0;
+
+	fd = open("input", O_RDONLY);
+	line = get_next_line(fd);
+	if (!line)
+		exit(EXIT_FAILURE);
+	while (line[i])
+	{
+		if ((result = check_marker(&line[i])) == 1)
+			return (printf("score: %d\n", i + 14) * 0);
+		i++;
+	}
+	close(fd);
+	return (0);
+}
